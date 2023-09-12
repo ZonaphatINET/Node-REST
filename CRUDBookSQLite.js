0bbecd7880bc162b0736414480a145273c955fe8
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-09-12 10:44:35 +0700

    update

--- CRUDBookSQLite.js
+++ CRUDBookSQLite.js
@@ -73,5 +73,5 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
