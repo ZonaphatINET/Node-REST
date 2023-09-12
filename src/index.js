f142253c46b06156b38a42e18867288376e6fb03
ZonaphatINET <s6506022420011@email.kmutnb.ac.th>
2023-09-12 10:48:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Hello server"); 
+  res.send("Hello World! Hello server!!"); 
 });
 
 app.listen(port, () => {
